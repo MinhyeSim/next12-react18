a17a9e21b1226f27ea8b69bdbb230a6b34c761f7
MinhyeSim <smine0032@gmail.com>
2022-04-26 21:22:26 +0900

    21:22

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -1,5 +1,5 @@
 import { HYDRATE } from "next-redux-wrapper"
-import * as T from "../types"
+import * as T from "@/store/types"
 
 const initialState = {
     users: [],
@@ -8,12 +8,15 @@ const initialState = {
 }
 
 const userReducer = (state = initialState, action) =>{
+    //alert(`2 >> `+JSON.stringify(loginInfo))
     switch (action.type){
         case HYDRATE:
             console.log(" ### Error: Hydration failed because the initial UI does not match what was rendered on the server. ### ")
             return { ...state, ...action.payload }
-        case T.USER_ADD_SUCCESSED:
+        case T.USER_ADD_SUCCESS:
             return { ...state, users: [action.payload, ...state.users] }
+
+
         default:
             return state;
     }
--- store/sagas/user.js
+++ store/sagas/user.js
@@ -7,17 +7,16 @@ function* addUser(action){
     try{
         const newUser = yield response.json()
         yield put({
-            type: T.USER_ADD_SUCCESSED,
+            type: T.USER_ADD_SUCCESS,
             payload: newUser.data
         }) 
     }catch(error){
         yield put({
-            type: T.USER_ADD_FAILED,
+            type: T.USER_ADD_FAIL,
             payload: error.message
         })
     }
 }
 export function* watchAddUser(){
-    yield takeLatest(T.USER_ADD_REQUESTED, addUser)
+    yield takeLatest(T.USER_ADD_REQUEST, addUser)
 }
-
